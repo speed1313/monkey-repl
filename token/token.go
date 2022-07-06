7fe1ca4b53b0d29976834a9a23ce7830ae413811
speed1313 <speedtry13@icloud.com>
2022-07-07 01:34:55 +0900

    implement reading only one character token

--- token/token.go
+++ token/token.go
@@ -2,36 +2,33 @@ package token
 
 type TokenType string
 
-type Token struct{
-    Type TokenType
-    Literal string
-
+type Token struct {
+	Type    TokenType
+	Literal string
 }
 
-
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF = "EOF
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    // identifier and literal
-    IDENT = "IDENT" // variable name, function name,..
-    INT = "INT" // integer
+	// identifier and literal
+	IDENT = "IDENT" // variable name, function name,..
+	INT   = "INT"   // integer
 
-    // operator
-    ASSIGN = "="
-    PLUS = "+"
+	// operator
+	ASSIGN = "="
+	PLUS   = "+"
 
-    // delimiter
-    COMMA = ","
-    SEMICOLON = ";"
+	// delimiter
+	COMMA     = ","
+	SEMICOLON = ";"
 
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    // keyword
-    FUNCTION = "FUNCTION"
-    LET = "LET"
+	// keyword
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
-
