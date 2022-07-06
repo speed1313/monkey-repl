98dc02ea3c7ab8f4cd17ed235060772a5484c87f
speed1313 <speedtry13@icloud.com>
2022-07-07 02:38:18 +0900

    impl simple main

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"github.com/speed1313/monkey-repl/repl"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
