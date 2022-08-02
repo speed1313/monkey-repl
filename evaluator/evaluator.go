679195668c029d38e4c57333087f11c9d2e98174
speed1313 <speedtry13@icloud.com>
2022-08-02 20:51:47 +0900

    impl prefix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -4,6 +4,12 @@ import (
 	"github.com/speed1313/monkey-repl/ast"
 	"github.com/speed1313/monkey-repl/object"
 )
+var (
+	NULL = &object.Null{}
+	TRUE = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object{
 	switch node := node.(type){
 		case *ast.Program:
@@ -12,6 +18,11 @@ func Eval(node ast.Node) object.Object{
 			return Eval(node.Expression)
 		case *ast.IntegerLiteral:
 			return &object.Integer{Value: node.Value}
+		case *ast.Boolean:
+			return nativeBoolToBooleanObject(node.Value)
+		case *ast.PrefixExpression:
+			right := Eval(node.Right)
+			return evalPrefixExpression(node.Operator, right)
 	}
 	return nil
 }
@@ -22,4 +33,42 @@ func evalStatements(stmts []ast.Statement) object.Object{
 		result = Eval(stmt)
 	}
 	return result
+}
+func nativeBoolToBooleanObject(input bool) *object.Boolean{
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object{
+	switch operator {
+		case "!":
+			return evalBangOperatorExpression(right)
+		case "-":
+			return evalMinusPrefixOperatorExpression(right)
+		default:
+			return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object{
+	switch right {
+		case TRUE:
+			return FALSE
+		case FALSE:
+			return TRUE
+		case NULL:
+			return TRUE
+		default:
+			return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object{
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
