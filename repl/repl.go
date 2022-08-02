6416dbe2cebfc291c7abada3165b33ed6870be20
speed1313 <speedtry13@icloud.com>
2022-08-02 20:39:41 +0900

    add evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/speed1313/monkey-repl/lexer"
 	"github.com/speed1313/monkey-repl/parser"
+	"github.com/speed1313/monkey-repl/evaluator"
 	"io"
 )
 
@@ -27,8 +28,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
