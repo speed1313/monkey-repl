f25c92eccea0c7b933b84f8662a2a46cd80d098e
speed1313 <speedtry13@icloud.com>
2022-08-04 19:05:36 +0900

    support builtin funciton

--- object/object.go
+++ object/object.go
@@ -15,6 +15,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type ObjectType string
@@ -92,3 +93,12 @@ func (s *String) Type() ObjectType {
 func (s *String) Inspect() string {
 	return s.Value
 }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ}
+func (b *Builtin) Inspect() string  { return "builtin function" }
